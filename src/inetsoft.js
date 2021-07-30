50799fd21e3085b5b50eff27b2e7d5683a10042f
Jason Shobe <jason.shobe@inetsoft.com>
2021-07-30 16:27:54 -0400

    Post message to parent when connected.

--- src/inetsoft.js
+++ src/inetsoft.js
@@ -1,22 +1,16 @@
-function isOriginAllowed(allowedOrigin, sourceOrigin) {
-    return (!!allowedOrigin && sourceOrigin === allowedOrigin) || (sourceOrigin == window.location.origin);
-}
-
-function loadData(url, token, callback) {
-    const options = { headers: { "X-XSRF-TOKEN": token } };
-    fetch(url, options)
-        .then(response => response.json())
-        .then(data => callback(data));
-}
-
 export default function(assemblyName, callback, origin) {
+    const allowedOrigin = origin || window.location.origin;
     window.addEventListener("message", event => {
-        if(isOriginAllowed(origin, event.orgin) && !!event.data && event.data.type === "inetsoftExternalUrls") {
+        if(event.origin === allowedOrigin && !!event.data && event.data.type === "inetsoftExternalUrls") {
             if(!!event.data.urls && Object.prototype.hasOwnProperty.call(event.data.urls, assemblyName)) {
                 const url = event.data.urls[assemblyName];
                 const token = event.data.token;
-                loadData(url, token, callback);
+                const options = { headers: { "X-XSRF-TOKEN": token } };
+                fetch(url, options)
+                    .then(response => response.json())
+                    .then(data => callback(data));
             }
         }
     });
+    window.parent.postMessage({type: "inetsoftGetExternalUrls"}, allowedOrigin);
 }
